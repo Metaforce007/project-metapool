d55b6fd5c0431f42348212e6bf282b589ad1fc64
Metaforce007 <Metaforce007@gmail.com>
2016-07-26 01:38:52 +0300

    Update extract_rgb.m

--- extract_rgb.m
+++ extract_rgb.m
@@ -3,7 +3,7 @@
 % $point is the point which we want to extract the rgb value from
 
 function [rgb] = extract_rgb(img, point)
-    rgb = [0,0,0];  % initialize rgb value
+    rgb = [0,0,0]; % initialize rgb value
     
     % the r\g\b is at the 3rd dimension of the image $img, so we are
     % referring to the same pixel 3 times, each time for each dimension
