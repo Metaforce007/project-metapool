dd192d91e13d8ef0adc321f3aad2c6537aac4773
Metaforce007 <Metaforce007@gmail.com>
2016-07-26 00:06:49 +0300

    Update ball_center.m

--- ball_center.m
+++ ball_center.m
@@ -15,20 +15,20 @@ function [center] = ball_center(img, point)
         for j=-1 : 2 : 1    % iterate -1:2:1    -> each for travelling 1 step forward (+x \ +y) or 1 step backwards (-x \ -y)
             
             n_travel = n_travel + 1;    % incrementing the current travel by 1
-            
+
             % while the color of $point is not 8 (not the table color)
             while interpret_rgb(extract_rgb(img, point)) ~= 8
                 
-                point(3-i) = point(3-i) - j;                % going 1 step forward \ backwards -> based on j
+                point(i) = point(i) - j;                % going 1 step forward \ backwards -> based on j
                 travel(n_travel) = travel(n_travel) + 1;    % incrementing the steps for the current travel by 1
             end
             
-            point(3-i) = point(3-i) + j*travel(n_travel);   % reseting the current point to its starting value
+            point(i) = point(i) + j*travel(n_travel);   % reseting the current point to its starting value
         end
         
         % calculating the center point(x or y) by adding the subtraction of
         % the backward steps from the forward steps, to the starting point
         % (x or y)
-        center(i) = point(3-i) + (travel(n_travel-1) - travel(n_travel))/2;
+        center(i) = point(i) + (travel(n_travel-1) - travel(n_travel))/2;
     end
 end
