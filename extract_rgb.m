fe55719edd5b60af3d72524f94b11493ec65e504
Metaforce007 <Metaforce007@gmail.com>
2016-07-25 23:45:43 +0300

    returns the rgb of $point in $img

---
+++ extract_rgb.m
@@ -0,0 +1,12 @@
+% extract_rgb(img, point) returns the rgb of $point in $img
+% $img is an image
+% $point is the point which we want to extract the rgb value from
+
+function [rgb] = extract_rgb(img, point)
+    rgb = [0,0,0];  % initialize rgb value
+    
+    % the r\g\b is at the 3rd dimension of the image $img, so we are
+    % referring to the same pixel 3 times, each time for each dimension
+    for i=1 : 3; rgb(i) = img(point(1), point(2), i);
+    end
+end
