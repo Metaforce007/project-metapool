8e665fbcb3b22a5e8aa6c99fb08a19a3ae6a441d
Metaforce007 <Metaforce007@gmail.com>
2016-07-26 01:37:58 +0300

    Update ball_center.m

--- ball_center.m
+++ ball_center.m
@@ -11,19 +11,28 @@ function [center] = ball_center(img, point)
     n_travel    = 0;        % counter that count the current travel
     center      = [0, 0];   % initializing center points
     
-    for i=1 : 2             % iterate 1:2       -> each for $point(1) and $point(2)
-        for j=-1 : 2 : 1    % iterate -1:2:1    -> each for travelling 1 step forward (+x \ +y) or 1 step backward (-x \ -y)
+    for i=1 : 2             % iterate 1:2, each for $point(1) and $point(2)
+        
+        % iterate -1:2:1, each for travelling 1 step forward (+x \ +y) or 1
+        % step backward (-x \ -y)
+        for j=-1 : 2 : 1
             
-            n_travel = n_travel + 1;    % incrementing the current travel by 1
+            n_travel = n_travel + 1; % incrementing the current travel by 1
 
             % while the color of $point is not 8 (not the table color)
             while interpret_rgb(extract_rgb(img, point)) ~= 8
                 
-                point(i) = point(i) - j;                % going 1 step forward \ backward -> based on j
-                travel(n_travel) = travel(n_travel) + 1;    % incrementing the steps for the current travel by 1
+                % going 1 step forward \ backward, based on j
+                point(i) = point(i) - j;
+                
+                % incrementing the steps for the current travel by 1
+                travel(n_travel) = travel(n_travel) + 1;
             end
             
-            point(i) = point(i) + j*travel(n_travel);   % reseting the current point to its starting value
+            % reseting the current point to its starting value
+            % adding \ subtracting the travel distance to \ from $point(1),
+            % based on j (+1 OR -1);
+            point(i) = point(i) + j*travel(n_travel);
         end
         
         % calculating the center point(x or y) by adding the subtraction of
