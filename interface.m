e7a85313bd92ebdaf97d3a9a57eaaf26d6b0d9c2
Metaforce007 <Metaforce007@gmail.com>
2016-07-27 00:37:12 +0300

    Update interface.m

--- interface.m
+++ interface.m
@@ -22,7 +22,7 @@ function varargout = interface(varargin)
 
 % Edit the above text to modify the response to help interface
 
-% Last Modified by GUIDE v2.5 26-Jul-2016 18:34:58
+% Last Modified by GUIDE v2.5 26-Jul-2016 23:50:53
 
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
@@ -56,9 +56,18 @@ function interface_OpeningFcn(hObject, eventdata, handles, varargin)
 handles.output = hObject;
 
 feed = imread('exm2.jpg');
-handles.feed = feed;
-axes(handles.table);
+h_feed = imagesc(feed);
+set(h_feed, 'ButtonDownFcn', {@click, handles});
+axes(handles.current_ball);
 imshow(feed);
+
+
+
+handles.selected_code = 0;
+handles.feed = feed;
+handles.avg_rgb = [0, 0, 0];
+colors_submitted = 0;
+handles.colors_submitted = colors_submitted;
 
 % Update handles structure
 guidata(hObject, handles);
@@ -80,10 +89,78 @@ varargout{1} = handles.output;
 
 % --- Executes on button press in select_area.
 function select_area_Callback(hObject, eventdata, handles)
+
+surface = getrect(handles.table);
+handles.surface = ceil(surface);
+
+guidata(hObject, handles);
+
 % hObject    handle to select_area (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
 
-selected_area = getrect;
-avg_rgb = average_rgb(handles.feed, ceil(selected_area));
-assignin('base', 'avg_rgb', avg_rgb);
+
+% --- Executes on selection change in codes_list.
+function codes_list_Callback(hObject, eventdata, handles)
+
+codes = cellstr(get(hObject, 'String'));
+selected_code = codes{get(hObject, 'Value')}(1) - 48;
+handles.selected_code = selected_code;
+guidata(hObject, handles);
+
+
+% hObject    handle to codes_list (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: contents = cellstr(get(hObject,'String')) returns codes_list contents as cell array
+%        contents{get(hObject,'Value')} returns selected item from codes_list
+
+
+% --- Executes during object creation, after setting all properties.
+function codes_list_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to codes_list (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: listbox controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+% --- Executes on button press in submit.
+function submit_Callback(hObject, eventdata, handles)
+
+avg_rgb_surface = average_rgb(handles.feed, handles.surface);
+handles.avg_rgb = avg_rgb_surface;
+
+assignin('base', strcat('code', num2str(handles.selected_code)), [handles.selected_code, handles.avg_rgb]);
+
+guidata(hObject, handles);
+
+% hObject    handle to submit (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+
+function click(hObject, eventdata, handles)
+
+exm32 = imread('exm2.jpg');
+pos = get(handles.table, 'CurrentPoint');
+pos = ceil(pos(1, 1:2));
+
+ball_identity = identify_ball(exm32, pos);
+
+crop_area = ball_identity(1:4);
+crop_area(3:4) = crop_area(3:4) - crop_area(1:2);
+
+img_ball = imcrop(exm32, crop_area);
+
+axes(handles.current_ball);
+imshow(img_ball);
+
+avg_rgb = ball_identity(5:7);
+handles.avg_rgb = avg_rgb;
+guidata(hObject, handles);
